155c6b6e6d4a6ca634e0a55f605c1418ef5784db
unknown <alificleiton123@hotmail.com>
2023-01-17 08:57:20 -0300

    Adding new files

---
+++ cabecalho.php
@@ -0,0 +1,70 @@
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  
+  <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+  <!-- CSS LINKS -->
+  <link rel="stylesheet" href="css/style.css"> 
+  <link rel="stylesheet" href="css/style2.css"> 
+ 
+  <title>ACS CURSOS ONLINE</title>
+</head>
+<body>
+
+
+  <header>
+     <!-- Navbar  -->
+     <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+      <div class="container">
+        <a class="navbar-brand" href="#main">ACS CURSOS ONLINE</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav text-center ">
+            <li class="nav-item">
+              <a class="nav-link text-white " href="#main">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#inscricao">INSCREVA-SE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#video_principal">SOBRE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#videos">VIDEOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#autor">AUTOR</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#FAQ">FAQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#contact">CONTATOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="login.php">| LOGIN</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
---
+++ login.php
@@ -0,0 +1,49 @@
+<!-- Cabeçalho ------------------------------------------- -->
+<? include_once "cabecalho.php"; ?>
+
+  <!-- seção de home  ------------------------------------------------->
+  <section class="menu" id="menu">
+    <div class="form">
+      <div class="form-content">
+        <header>Login</header>
+          <form action="#">
+            <div class="field input-field">
+              <input type="email" placeholder="Email" class="input">
+            </div>
+            <div class="field input-field">
+              <input type="password" placeholder="Password" class="password">
+              <i class="bx bx-hide eye-icon"></i>
+            </div>
+            <div class="form-link">
+              <a href="#" class="forgot-pass">Forgot password?</a>
+            </div>
+            <div class="field button-field">
+              <button>Login</button>
+            </div>
+          </form>
+          <div class="form-link">
+            <span>Don't have an account?<a href="singup.html">Signup</a></span>
+          </div>
+          
+          <div class="line"></div>
+
+          <ul class="sci">
+            <li><img src="imagens/face.png" width="40px"></li>
+            <li><img src="imagens/insta.png" width="40px"></li>
+            <li><img src="imagens/twiter.png" width="40px"></li>
+          </ul>
+          
+      </div>
+    </div>
+  </section>
+
+
+  <footer class="footer">
+    <span class="footer-title">Alifi Cleiton</span>
+    <p>Copyright 2023<br><a href="#">ACS CURSOS ONLINE</a> <br>All Rights Reserved.</p>
+  </footer>
+
+  <script type="text/javascript" src="js/script.js"></script>
+    
+</body>
+</html>
